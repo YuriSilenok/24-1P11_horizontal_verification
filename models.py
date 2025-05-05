f74d42a4e1fe90578d51a263fc596a7b444c5b79
Bormerd <saskey908@gmail.com>
2025-05-05 16:31:49 +0300

    .

--- models.py
+++ models.py
@@ -1,18 +1,28 @@
-from peewee import SqliteDatabase, Model, CharField, BooleanField
+"""База данных для горизонтальной проверки"""
+
 from enum import Enum
-from datetime import datetime, timezone
+from peewee import SqliteDatabase, Model, CharField, BooleanField
+
 
 db = SqliteDatabase("test.db")
 
+
 class UserRole(str, Enum):
+    """Роли пользователей в системе"""
     STUDENT = "student"
     TEACHER = "teacher"
 
+
 class BaseModel(Model):
+    """Базовая модель Peewee для всех моделей приложения."""
+
     class Meta:
+        """Мета-класс для указания используемой базы данных."""
         database = db
 
+
 class User(BaseModel):
+    """Модель пользователя системы"""
     username = CharField(unique=True)
     email = CharField()
     full_name = CharField()
@@ -20,9 +30,12 @@ class User(BaseModel):
     disabled = BooleanField(default=False)
     role = CharField(default=UserRole.STUDENT.value)
 
+
 def initialize_database():
+    """Инициализирует базу данных и создает необходимые таблицы"""
     with db:
         db.create_tables([User])
+
 
 if __name__ == "__main__":
     initialize_database()
